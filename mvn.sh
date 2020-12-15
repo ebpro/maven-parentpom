f1dc35ae33112b48dc03fe252bb3217931ea7f28
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2020-12-15 19:57:24 +0100

    moves to maven docker script.

---
+++ mvn.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+docker run \
+          -e GITHUBLOGIN=$GITHUBLOGIN \
+          -e GITHUBPASSWORD=$GITHUBPASSWORD \
+          -v ~/.m2:/var/maven/.m2 \
+          -v "$(pwd)":/usr/src/mymaven \
+          -w /usr/src/mymaven \
+          --rm \
+          -u `id -u` \
+          -e MAVEN_CONFIG=/var/maven/.m2 \
+          maven:3.6.3-jdk-11-openj9 \
+          mvn -B -e -T 1C -Duser.home=/var/maven --settings /usr/src/mymaven/.github/ci-settings.xml "$@"
