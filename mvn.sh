23b272529d5d31c9ac254b0c751834a6a5cff2d9
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2021-01-04 15:37:50 +0100

    moves to full maven support in docker including sonar.

--- mvn.sh
+++ mvn.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 docker run \
-          -e GITHUBLOGIN=$GITHUBLOGIN \
-          -e GITHUBPASSWORD=$GITHUBPASSWORD \
-          -v ~/.m2:/var/maven/.m2 \
-          -v "$(pwd)":/usr/src/mymaven \
-          -w /usr/src/mymaven \
+          --env GITHUBLOGIN=$GITHUBLOGIN \
+          --env GITHUBPASSWORD=$GITHUBPASSWORD \
+          --volume ~/.m2:/var/maven/.m2 \
+          --volume ~/.ssh:/home/user/.ssh \
+          --volume ~/.gitconfig:/home/user/.gitconfig \
+          --volume "$(pwd)":/usr/src/mymaven \
+          --workdir /usr/src/mymaven \
           --rm \
-          -u `id -u` \
-          -e MAVEN_CONFIG=/var/maven/.m2 \
-          maven:3.6.3-jdk-11-openj9 \
-          mvn -B -e -T 1C -Duser.home=/var/maven --settings /usr/src/mymaven/.github/ci-settings.xml "$@"
+          --env PUID=`id -u` -e PGID=`id -g` \
+          --env MAVEN_CONFIG=/var/maven/.m2 \
+          brunoe/maven:3.6.3-jdk-11-openj9 \
+          runuser --user user --group user -- mvn -B -e -T 1C -Duser.home=/var/maven --settings /usr/src/mymaven/.github/ci-settings.xml "$@"
