362012102e5b8ee67b9518c33a5ed5b73be8c77c
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-05-11 18:10:37 +0200

    revert maven default version

---
+++ build/host_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## MAKE SURE YOU HAVE enviroment variables
+# GITHUB_ACTOR
+# GITHUB_TOKEN
+# DOCKERHUB_USERNAME
+# DOCKERHUB_TOKEN
+# SONAR_TOKEN
+
+mvn --errors \
+    --show-version \
+    --batch-mode \
+    org.apache.maven.plugins:maven-wrapper-plugin:3.2.0:wrapper \
+        "-Dmaven=3.8.8"
+
+./mvnw --errors \
+              --show-version \
+              --batch-mode \
+              --update-snapshots \
+              --color always \
+              --settings ci-settings.xml \
+              --activate-profiles jacoco \
+              verify
+
+ ./mvnw --errors \
+              --show-version \
+              --batch-mode \
+              --color always \
+              --settings ci-settings.xml \
+              site:site 
